e4bb4b72a08583ee1f12974cf4e5d07c62850e3e
Baguette <veryveroniquevery@gmail.com>
2023-03-06 21:46:40 +0100

    Modified my index.js, added some more styles and made more responsive to make it accessible from other devices like desktops (not finished yet)

--- brief#2/js/script.js
+++ brief#2/js/script.js
@@ -4,21 +4,24 @@ window.addEventListener('scroll', () => {
     const header = document.querySelector("header");
     const headerTitle = document.querySelector("header div p");
     const headerMenuButton = document.getElementsByClassName("material-symbols-outlined")[0];
-    const headerMenuText = document.querySelectorAll("header #menu ul li a");
+    const headerMenuText = document.querySelectorAll("#nav-menu ul li a");
     if(window.scrollY > 20){
         header.classList.add("header-background");
         headerTitle.classList.add("header-title");
-        // for (var li_index in headerMenuText){
-        //     headerMenuText[li_index].classList.add("header-text-menu-scroll");
-        // }
+        for (var li_index in headerMenuText){
+            if (typeof headerMenuText[li_index] == "object")
+            headerMenuText[li_index].classList.add("header-text-menu-scroll");
+            
+        }
         
     }else{
         header.classList.remove("header-background");
         headerTitle.classList.remove("header-title");
         headerMenuButton.classList.remove("header-title");
-        // for (var li_index in headerMenuText){
-        //     headerMenuText[li_index].classList.remove("header-text-menu-scroll");
-        // }
+        for (var li_index in headerMenuText){
+            if (typeof headerMenuText[li_index] == "object")
+            headerMenuText[li_index].classList.remove("header-text-menu-scroll");
+        }
     }
 });
 
