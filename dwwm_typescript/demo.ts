bbfaede3df57416f68ca05ff4b69ba8053db93e1
Baguette <veryveroniquevery@gmail.com>
2023-03-09 11:46:59 +0100

    New exercices with functions, JSON objects (interfaces) and conditions :smirk:

--- dwwm_typescript/demo.ts
+++ dwwm_typescript/demo.ts
@@ -60,3 +60,157 @@ function exercice_4_revert(random_string: string[]){
 
 
 
+
+
+
+
+
+/*
+Exercice 1:
+============
+*/
+
+function exercice_1_data(){
+    const data: string[] = ['Théo', 'Martin', 'Lucas', 'Antoine', 'Etienne']
+    for(let i: number = 0; i < data.length; i++){
+        if(data[i].length <= 5){
+            console.log(`Le prénom ${data[i]} est inférieur ou égal à 5 caractères.`);
+            
+        }
+    }
+}
+
+// exercice_1_data()
+/*
+Exercice 2:
+============
+*/
+
+function exercice_2_countries(){
+    const countries: string[] = ["France", "Allemagne", "Italie", "Suisse", "Belgique"]
+    var sentence: string = ""
+    sentence = countries.join(', ')
+    console.log(sentence)
+    return sentence    
+}
+
+// exercice_2_countries()
+
+/*
+Exercice 3:
+============
+*/
+
+function exercice_3_numbers(){
+    const numbers: number[] = [123, 999, 340, 12390]
+    const results: number[] = []
+    for (let i:number = 0; i < numbers.length; i++){
+        results.push(numbers[i] * 180)
+    }
+    console.log(results)
+    return results
+    
+}
+
+// exercice_3_numbers()
+
+/*
+Exercice 4:
+============
+*/
+
+function exercice_4_objet(){
+    interface Objet {
+        name: string,
+        surname: string,
+        age: number
+    }
+
+    const objet: Objet = {
+        name: 'Marchal',
+        surname: 'Mickeal',
+        age: 19
+    }
+
+    objet.age >= 18 ? console.log(`${objet.name} ${objet.surname} est majeur.`) : console.log(`${objet.name} ${objet.surname} n'est pas majeur!`);
+    
+}
+
+// exercice_4_objet()
+/*
+Exercice 5:
+============
+*/
+
+function exercice_5_un_objet(){
+    interface UnObjet{
+        nom: string,
+        prenom: string,
+        age: number
+    }
+
+    const personne: UnObjet = {
+        nom: "Lassalle",
+        prenom: "Jean",
+        age: 10
+    }
+
+    console.log(`Salut, j'suis ${personne.prenom} ${personne.nom} et je suis agé de ${personne.age} ans`);
+    
+}
+
+// exercice_5_un_objet()
+
+/*
+Exercice 6:
+============
+*/
+
+function exercice_6_gens(){
+    interface Personne{
+        nom: string,
+        prenom: string,
+        age: number
+    }
+
+    const personnes: Personne[] = [
+        {
+            nom: "asd",
+            prenom: "dsa",
+            age: 16
+        },
+        {
+            nom: "asd1",
+            prenom: "dsa1",
+            age: 19
+        },
+        {
+            nom: "asd2",
+            prenom: "dsa2",
+            age: 27
+        },
+        {
+            nom: "asd3",
+            prenom: "dsa3",
+            age: 49
+        },
+        {
+            nom: "asd4",
+            prenom: "dsa4",
+            age: 5
+        },
+    ]
+
+    for (let i = 0; i < personnes.length; i++){
+        let personne = personnes[i]
+        
+        personne.age >= 18
+        ?
+        console.log(`Bonjour, mon nom est ${personne.nom} et mon prénom est ${personne.prenom}. Je suis majeur car je suis agé de ${personne.age} ans`)
+        :
+        console.log(`Bonjour, je m'appelle ${personne.nom} ${personne.prenom} et je suis mineur car je suis agé de ${personne.age} ans mdr`);
+    }
+}
+
+
+// exercice_6_gens()
