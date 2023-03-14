cae509c06974ea9adf0a8677d757dc60aefe7b1e
Zen-kun04 <veryveroniquevery@gmail.com>
2023-03-14 11:02:05 +0100

    update

---
+++ dwwm_typescript/exo_dom/exercice_1/script.ts
@@ -0,0 +1,28 @@
+// Exercice 1
+
+// Récupérer l'element du DOM contenant la phrase "Cible moi 🎯" à l'aide d'un getElementById
+// Affichez l'element dans la console
+
+// Exercice 2
+
+// Récupérer les elements du DOM contenant la phrase "Cible moi 🎯" à l'aide d'une getElementsByClassName
+// Affichez les elements dans la console
+
+// Exercice 3
+
+// Écouter le click de l'utilisateur sur le bouton "Hello !" à l'aide d'un eventListener
+// À chaque click vous afficherez "Hey dude!" dans la console
+
+// Exercice 4
+
+// Écouter le click de l'utilisateur sur les boutons "+" et "-"
+// À chaque click vous augmenterez / diminuerez une variable contenant un chiffre et l'afficherez dans la console
+
+// Exercice 5.1
+
+// Écouter le text saisi par l'utilisateur dans l'input
+// À chaque modification vous afficherez la valeur de l'input dans la console
+
+// Exercice 5.2
+
+// Remplacer le paragraph contenant "Remplacer par la saisie de l'utilisateur" par la valeur de l'input en direct
