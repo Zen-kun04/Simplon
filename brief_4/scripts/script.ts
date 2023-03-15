11f8fd02d4717892243b90b06a45671f4dd64c43
Zen-kun04 <veryveroniquevery@gmail.com>
2023-03-15 21:30:06 +0100

    2023-03-15 - Made brief_4 much more dynamic

--- brief_4/scripts/script.ts
+++ brief_4/scripts/script.ts
@@ -12,31 +12,32 @@ import config from '../config.json';
 //   }
 // }
 
+interface Question {
+    title: string,
+    answers: string[],
+    correct: number
+}
 
 
-const jsonQuestions: any = config.questions;
+const jsonQuestions: Question[] = config.questions;
 console.log(jsonQuestions);
 
-const answerButtonList = document.getElementsByClassName("answer");
-const answerButtonListArray = Array.from(answerButtonList);
+
 const title = document.getElementById("title")!;
 let completed_questions: Object[] = [];
 
 let points = 0;
 
 function nextQuestion(){
+    const answerButtonList = document.getElementsByClassName("answer");
+    const answerButtonListArray = Array.from(answerButtonList);
   if(completed_questions.length === jsonQuestions.length){
     title.innerHTML = "T'as fini!"
-    for(let i = 0; i < answerButtonListArray.length; i++){
-      let button = answerButtonListArray[i] as HTMLElement;
-      button.style.backgroundColor = "unset";
-      button.style.boxShadow = "unset";
-      button.innerText = "";
-    }
+    hideAnswers();
     console.log(points);
     
-  }
-  for(let question of jsonQuestions){
+  }else{
+    for(let question of jsonQuestions){
     
     if(!completed_questions.includes(question)){
       title.innerText = question.title;
@@ -49,21 +50,110 @@ function nextQuestion(){
       break;
     }
   }
+  }
+  
 
 }
+
+function buttonListener(){
+    const answerButtonList = document.getElementsByClassName("answer");
+    const answerButtonListArray = Array.from(answerButtonList);
+    for(let i = 0; i < answerButtonListArray.length; i++) {
+        const answer = answerButtonListArray[i] as HTMLElement;
+        answer.addEventListener('click', () => {
+          const currentQuestion = getCurrentQuestion();
+          
+          if(answerButtonListArray.indexOf(answer) === currentQuestion.correct) points++;
+          nextQuestion();
+        });
+      }
+}
+
+let colors = ["red", "purple", "green", "blue"];
+
+function createButton(){
+    const answersDiv: Element | null = document.getElementById("answers");
+    const buttonCreation: Element = document.createElement("div")
+    buttonCreation.classList.add("answer");
+    for(let color of colors){
+        buttonCreation.id = color;
+        colors.splice(colors.indexOf(color), 1);
+        break;
+    }
+    answersDiv?.appendChild(buttonCreation);
+    return buttonCreation;
+}
+
+function loadQuestion(question: Question){
+    title.textContent = question.title;
+    for(let answer of question.answers){
+        let button: Element = createButton();
+        button.textContent = answer;
+    }
+    colors = ["red", "purple", "green", "blue"];
+}
+
+function startQuiz(){
+    // buttonListener();
+    var i = 0;
+    var temps = 5 // en secondes
+
+    function executeQuestion(){
+        var quest = jsonQuestions[i];
+        loadQuestion(quest);
+        var quiz = setInterval(function(){
+            temps--;
+            console.log(temps);
+            
+            if(temps === 0){
+                hideAnswers();
+                console.log("T'as perdu");
+                clearInterval(quiz);
+                temps = 5;
+                i++;
+                if(i < jsonQuestions.length){
+                    executeQuestion();
+                }else{
+                    console.log("Quiz terminé");
+                    title.innerHTML = "T'as fini!"
+                    console.log(points);
+                    
+                }
+                
+            }
+        }, 1000);
+    }
+    executeQuestion();
+        
+        
+}
+    // nextQuestion();
+
 
 function getCurrentQuestion(){
   const queue: number = completed_questions.length;
   return jsonQuestions[queue - 1];
 }
 
-nextQuestion();
-for(let i = 0; i < answerButtonListArray.length; i++) {
-  const answer = answerButtonListArray[i] as HTMLElement;
-  answer.addEventListener('click', () => {
-    const currentQuestion = getCurrentQuestion();
-    
-    if(answerButtonListArray.indexOf(answer) === currentQuestion.correct) points++;
-    nextQuestion();
-  });
+function hideAnswers(){
+    const answerButtonList = document.getElementsByClassName("answer");
+    const answerButtonListArray = Array.from(answerButtonList);
+    for(let i = 0; i < answerButtonListArray.length; i++){
+        let button = answerButtonListArray[i] as HTMLElement;
+        button.remove();
+    }
 }
+
+function askForUsername(){
+    const div = document.createElement("p");
+    div.id = "username-div";
+    
+    const contentDiv = document.getElementById("content");
+    contentDiv?.appendChild(div);
+}
+
+
+// askForUsername();
+startQuiz();
+// startQuiz();
+
--- dwwm_typescript/realisations/exercice3.ts
+++
@@ -1,6 +0,0 @@
-const organism: string = "Simplon";
-const learnerss: number = 15;
-const promotionStarted: boolean = true;
-const learnerNames: string[] = ["Vincent", "Toufik", "Tom", "Samuel", "Orhan"];
-const pedagogicalReferent: Object  = { name: "Tiphaine", work: "Simplon" };
-const absentLearner: undefined = undefined;
