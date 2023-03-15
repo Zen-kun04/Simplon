aad3859d0f1cc750ceaebd934ca3bd33a011a79f
Zen-kun04 <veryveroniquevery@gmail.com>
2023-03-15 11:01:13 +0100

    Updated script.ts

--- dwwm_typescript/exo_dom/exercice_2/script.ts
+++ dwwm_typescript/exo_dom/exercice_2/script.ts
@@ -16,3 +16,77 @@
 
 // Lorsque l'utilisateur appuie sur une touche qui est une lettre de l'alphabet, 
 // appliquez la même logique que lorsqu'il click sur une lettre de l'alphabet ( background rouge ... )
+
+const voyelles = ["a", "e", "i", "o", "u"]
+const consonnes = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+let lettres_activees: HTMLElement[] = [];
+const lettres: HTMLCollectionOf<Element> = document.getElementsByClassName("letter");
+const lettresArray: Element[] = Array.from(lettres);
+
+
+function exo_1(){
+    for(let i = 0; i < lettresArray.length; i++){
+        let lettre = lettresArray[i] as HTMLElement;
+        lettre.addEventListener('click', () => {
+            if(lettres_activees.includes(lettre)){
+                lettre.style.backgroundColor = "unset";
+                lettres_activees.splice(lettres_activees.indexOf(lettre), 1);
+            }else{
+                lettre.style.backgroundColor = "gray";
+                lettres_activees.push(lettre);
+            }
+            
+            
+        })
+    }
+
+}
+
+// exo_1();
+
+
+function exo_2(){
+    
+    document.addEventListener('keypress', (key) => {
+        let key_value = key.key;
+        
+        if(voyelles.includes(key_value.toLowerCase())){
+            console.log("voyelle");
+            
+            for(let i = 0; i < lettresArray.length; i++){
+                let lettre = lettresArray[i] as HTMLElement;
+                if (lettre.textContent?.toLowerCase() === key_value.toLowerCase()){
+                    if(lettres_activees.includes(lettre)){
+                        lettre.style.backgroundColor = "unset";
+                        lettres_activees.splice(lettres_activees.indexOf(lettre), 1);
+                    }else{
+                        lettre.style.backgroundColor = "gray";
+                        lettres_activees.push(lettre);
+                    }
+                }
+            }
+            
+        }else if(consonnes.includes(key_value.toLowerCase())){
+            console.log("consonne");
+            
+            for(let i = 0; i < lettresArray.length; i++){
+                let lettre = lettresArray[i] as HTMLElement;
+                if (lettre.textContent?.toLowerCase() === key_value.toLowerCase()){
+                    if(lettres_activees.includes(lettre)){
+                        lettre.style.backgroundColor = "unset";
+                        lettres_activees.splice(lettres_activees.indexOf(lettre), 1);
+                    }else{
+                        lettre.style.backgroundColor = "gray";
+                        lettres_activees.push(lettre);
+                    }
+                }
+                
+            }
+        }else {
+            console.log("Pas dans l'alphabet");
+            
+        }
+    })
+}
+
+exo_2();
