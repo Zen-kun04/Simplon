57a403406383edb416059c6910806065d00a16f4
Zen-kun04 <veryveroniquevery@gmail.com>
2023-03-15 10:54:50 +0100

    New exercices (events) in TypeScript

---
+++ dwwm_typescript/exo_dom/exercice_2/script.ts
@@ -0,0 +1,18 @@
+// Exercice 1
+
+// Récupérez tous les élements du DOM contenant une lettre de l'alphabet
+// Vous écouterez le click du l'utilisateur sur chaque element ( boucle ? )
+// Lorsque l'utilisateur click sur une lettre faite en sorte que la lettre selectionnée ai un background gris
+// Si l'utilisateur re-click sur une touche déjà selectionnée remettez lui un background blanc
+
+// Exercice 2.1
+
+// Vous écouterez le clavier de l'utilisateur
+// Si il appuie sur une touche avec une voyelle, affichez "voyelle" dans la console
+// Si il appuie sur une touche avec une consonne, affichez "consonne" dans la console
+// Si il appuie sur une touch qui n'est ni une voyelle ni une consonne, affichez "Pas dans l'alphabet" dans la console
+
+// Exercice 2.2
+
+// Lorsque l'utilisateur appuie sur une touche qui est une lettre de l'alphabet, 
+// appliquez la même logique que lorsqu'il click sur une lettre de l'alphabet ( background rouge ... )
