bd7317275cc20ed147b8337fcdb41ccc7e3404ed
Zen-kun04 <veryveroniquevery@gmail.com>
2023-03-16 09:23:57 +0100

    2023-03-16 - new exercices

---
+++ dwwm_typescript/exo_dom/exercice_1.5/script.ts
@@ -0,0 +1,31 @@
+// Exercice 1
+
+// Lorsque l'utilisateur clique sur le bouton vous ajoutez la class "green" au carré
+
+
+// Exercice 2
+
+// Lorsque l'utilisateur clique sur le bouton :
+// - Si le carré est couleur coral, vous enlevez la class "coral" et ajoutez la class "green"
+// - Si le carré est couleur green, vous enlevez la class "green" et ajoutez la class "coral"
+
+
+// Exercice 3
+
+// Lorsque l'utilisateur clique sur le bouton vous récuperez la valeur de l'input
+// Selon la réponse l'utilisateur vous remplacez le text "Votre réponse est ..." du paragraphe par "Bonne" ou "Mauvaise"
+
+
+// Exercice 4
+
+// Lorsque l'utilisateur clique sur un carré coral celui-ci devient vert et les autres coral
+// Si l'utilisateur clique sur un carré vert, il ne se passe rien
+
+
+// Exercie 5
+
+// Lorsque l'utilisateur selectionne une liste, replacez la liste bidon par les membres de l'équipe sélectionnée
+
+const equipeA: string[] = ['Martin', 'Lucie', 'Jasmine']
+const equipeB: string[] = ['Lisa', 'Mathieu', 'Anthony']
+const equipeC: string[] = ['Anissa', 'Laïla', 'Jean-Paul']
