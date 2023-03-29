82c15635e6b67ba57a76ff0932907d74074cf85d
Zen-kun04 <veryveroniquevery@gmail.com>
2023-03-29 22:55:54 +0200

    2023-03-29 - function to correctly appear a character in the right place and centered

--- brief_5/scripts/modules/PlayingScreen.ts
+++ brief_5/scripts/modules/PlayingScreen.ts
@@ -1,5 +1,23 @@
 import { getCurrentCharacter } from "./CharacterContainers";
 
+function appearCharacter(currentCharacter: HTMLElement) {
+    const divCharacter: HTMLElement = document.createElement('div');
+    const characterNameParagraph: HTMLElement = document.createElement('p');
+    const characterSkinImg: HTMLImageElement = document.createElement('img');
+
+    // Set classes, ids, etc to new elements
+    divCharacter.id = 'character';
+    characterNameParagraph.textContent = currentCharacter.textContent!.trim();
+    characterSkinImg.src = './steve.png'
+    // Assign elements into each other
+    divCharacter.appendChild(characterNameParagraph);
+    divCharacter.appendChild(characterSkinImg);
+    document.getElementsByTagName('main')[0].appendChild(divCharacter);
+}
+
+function showHotBar(){
+    
+}
 
 export async function showPlayingScreen(){
     const currentCharacter: HTMLElement | null = getCurrentCharacter();
@@ -16,5 +34,6 @@ export async function showPlayingScreen(){
             oldHtml.replaceWith(newDoc.documentElement);
         }
         document.querySelector('main')?.appendChild(h1);
+        appearCharacter(currentCharacter);
     }
 }
