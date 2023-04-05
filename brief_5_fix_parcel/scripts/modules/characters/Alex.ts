9327b5d48536f2d1456f205f4702498351490b7c
Zen-kun04 <veryveroniquevery@gmail.com>
2023-04-05 23:20:17 +0200

    2023-04-05 - removed a testing fix for brief 5

--- brief_5_fix_parcel/scripts/modules/characters/Alex.ts
+++
@@ -1,9 +0,0 @@
-import { Character } from "../Character";
-
-export class Alex extends Character{
-    constructor(){
-        super("Alex");
-    }
-    a = this.getTaskName();
-    b = this.getTaskSpeed();
-}
--- brief_5_fix_parcel/scripts/modules/characters/Steve.ts
+++
@@ -1,9 +0,0 @@
-import { Character } from "../Character";
-
-export class Steve extends Character{
-    constructor(){
-        super("Steve");
-    }
-    a = this.getTaskName();
-    b = this.getTaskSpeed();
-}
